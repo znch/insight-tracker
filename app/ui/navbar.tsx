6336e17db6cd9598b73b3d709acb9e5c411c0a0e
Jan Koláček <kolacek.jenik@gmail.com>
2026-06-01 13:46:19 +0200

    nav + fonts + setup

---
+++ app/ui/navbar.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link"
+
+
+const eyeSlash = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="size-8">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+</svg>
+
+
+export default function Navbar() {
+    return(
+        <>
+        <nav className="bg-black w-full flex text-white px-4 py-2">
+            <div id="anchor" className="flex">
+                <a href="/" className="inline-flex text-inherit gap-3 items-center">
+                {eyeSlash} 
+                <span className="invisible sm:visible flex text-3xl items-center tracking-widest select-none"><span className="text-lg mr-1">iN</span>SiGhT</span>    
+                </a>
+                </div>
+            <div id="content"></div>
+        </nav>
+        </>
+    )
+}
